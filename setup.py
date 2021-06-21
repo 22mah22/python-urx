03f470c91aa6b746075e09ed5fe11b5588431380
magne <m.hovdar@gmail.com>
2021-06-21 15:34:40 +0200

    changed name for use at robotlab

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name="urx",
-    version="0.11.0",
+    name="ur3_robotlab",
+    version="0.11.2",
     description="Python library to control an UR robot",
     author="Olivier Roulet-Dubonnet",
     author_email="olivier.roulet@gmail.com",
     url='https://github.com/oroulet/python-urx',
-    packages=["urx"],
-    provides=["urx"],
+    packages=["ur3_robotlab"],
+    provides=["ur3_robotlab"],
     install_requires=["numpy", "math3d"],
     license="GNU Lesser General Public License v3",
     classifiers=[
